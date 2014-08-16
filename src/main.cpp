f480849082a88a460a5c91255472bbc078e7fa63
rawcoincomm <rawcointakeover@gmail.com>
2014-08-16 09:21:08 +0100

    rollback the fork

--- src/main.cpp
+++ src/main.cpp
@@ -986,17 +986,7 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 {
     int64_t nRewardCoinYear;
 
-     if (pindexBest->nHeight < 69000)
-
-     {
              nRewardCoinYear = MAX_MINT_PROOF_OF_STAKE;
-     }
-
-     else
-
-     {
-         nRewardCoinYear = NEW_MAX_MINT_PROOF_OF_STAKE;
-     }
 
 
     int64_t nSubsidy = nCoinAge * nRewardCoinYear / 365 / COIN;
